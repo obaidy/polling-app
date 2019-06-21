0509f468d6a1f565a7a3ee586a2f25ac6f7a4847
Ahmed Al-Obaidy <ahmed@A.local>
2019-06-21 10:43:55 +0200

    poll changes

---
+++ database/migrations/2019_06_21_083318_create_poll_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePollTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('poll', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('question');
+            // $table->integer('maxCheck')->default(1);
+            $table->boolean('canVisitorsVote')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('poll');
+    }
+}
